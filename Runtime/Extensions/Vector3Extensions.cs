941496ba6cda95ecb5ecef82dd09fc9f819f911b
Sebastien Berton <sebasti1.berton@gmail.com>
2024-04-07 06:51:00 +0200

    Fix bugs V3 extensions

--- Runtime/Extensions/Vector3Extensions.cs
+++ Runtime/Extensions/Vector3Extensions.cs
@@ -7,12 +7,12 @@ namespace Holypastry.Bakery
 
         public static Vector3 With(this Vector3 vector, float? x = null, float? y = null, float? z = null)
         {
-            return new Vector3(x ?? vector.X, y ?? vector.Y, z ?? vector.Z);
+            return new Vector3(x ?? vector.x, y ?? vector.y, z ?? vector.z);
         }
 
         public static Vector3 Add(this Vector3 vector, float? x = null, float? y = null, float? z = null)
         {
-            return new Vector3(vector.X + (x ?? 0), vector.Y + (y ?? 0), vector.Z + (z ?? 0));
+            return new Vector3(vector.x + (x ?? 0), vector.y + (y ?? 0), vector.z + (z ?? 0));
         }
 
     }
