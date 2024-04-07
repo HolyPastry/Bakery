912f4a1a6a4a866891b5b68335dcc3d7a8fadaf6
Sebastien Berton <sebasti1.berton@gmail.com>
2024-04-07 15:52:10 +0200

    Fix Spawnable namespace

--- Runtime/SpawnSystem/Spawnable.cs
+++ Runtime/SpawnSystem/Spawnable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Holypastry
+namespace Holypastry.Bakery
 {
     public abstract class Spawnable : MonoBehaviour
     { }
