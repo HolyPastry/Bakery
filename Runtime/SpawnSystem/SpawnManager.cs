c9686ae50ff8e0f9839693188b6e1a315d49d571
Sebastien Berton <sebasti1.berton@gmail.com>
2024-04-07 07:33:27 +0200

    fixing SpawnManager

--- Runtime/SpawnSystem/SpawnManager.cs
+++ Runtime/SpawnSystem/SpawnManager.cs
@@ -1,33 +1,28 @@
 using UnityEngine;
 
-namespace Holypastry.Bakery 
+namespace Holypastry.Bakery
 {
     public abstract class SpawnManager : MonoBehaviour
     {
 
-        [SerializeField] protected SpawnPointStrategyType spawnPointStrategyType = SpawnPointStrategyType.Linear;
-
         [SerializeField] protected Transform[] spawnPoints;
 
-        protected ISpawnStrategy _spawnStrategy;
+        [SerializeField] protected ISpawnStrategy _spawnStrategy;
 
-        protected enum SpawnPointStrategyType
-        {
-            Linear,
-            Random
-        }
 
         public virtual void Awake()
         {
-            switch (spawnPointStrategyType)
-            {
-                case SpawnPointStrategyType.Linear:
-                    _spawnStrategy = new LinearSpawnStrategy(spawnPoints);
-                    break;
-                case SpawnPointStrategyType.Random:
-                    _spawnStrategy = new RandomSpawnStrategy(spawnPoints);
-                    break;
-            }
+
+
+            // switch (spawnPointStrategyType)
+            // {
+            //     case SpawnPointStrategyType.Linear:
+            //         _spawnStrategy = new LinearSpawnStrategy(spawnPoints);
+            //         break;
+            //     case SpawnPointStrategyType.Random:
+            //         _spawnStrategy = new RandomSpawnStrategy(spawnPoints);
+            //         break;
+            // }
         }
 
         public abstract void Spawn();
